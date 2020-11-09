b79394ee6e7b82744a9450804a9f60c219b97ce7
lqwangxg <lqwangxg@gmail.com>
2020-11-09 01:57:58 +0000

    commit

---
+++ go/src/prj/go.mod
@@ -0,0 +1,11 @@
+module prj
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+)
---
+++ go/src/prj/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+"net/http"
+"github.com/labstack/echo"
+"github.com/labstack/echo/middleware"
+)
+
+func main() {
+   e := echo.New()
+
+   e.Use(middleware.Logger())
+   e.Use(middleware.Recover())
+   e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+     AllowOrigins: []string{"*"},
+     AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.POST, echo.DELETE},
+   }))
+   e.GET("/", func(c echo.Context) error {
+     return c.String(http.StatusOK,"Hello, wolrd!, this is from server written by golang.")
+   })
+   e.Logger.Fatal(e.Start(":8000"))
+
+}
+
+
